81781810108ad80286e6e987c2146cd592f0b811
gug007 <gurgen.abgayan@gmail.com>
2025-06-27 17:41:22 +0400

    Refactor Next.js configuration, update Tailwind CSS dependencies, and enhance README with deployment instructions. Remove unused files and streamline global styles.

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  basePath: '/todo-list-bot',
-  assetPrefix: '/todo-list-bot',
-  images: {
-    unoptimized: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
