a56de1d3df0fe1728c4b714c0d9ae0ffff73a455
gug007 <gurgen.abgayan@gmail.com>
2025-06-27 16:33:48 +0400

    Update Next.js configuration to set base path and asset prefix for deployment.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/todo-list-bot',
+  assetPrefix: '/todo-list-bot',
   images: {
     unoptimized: true,
   },
